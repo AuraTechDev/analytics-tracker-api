f579844f5a56fc29cf30d4a1a053875a0bbdbf7f
Erick Pac <erick.pac@helloiconic.com>
2025-02-11 17:13:14 -0600

    refactor: use ConfigService for environment variable access in DynamoDBService

--- src/db/dynamodb.service.ts
+++ src/db/dynamodb.service.ts
@@ -1,25 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class DynamoDBService {
   private readonly client: DynamoDBClient;
   private readonly docClient: DynamoDBDocumentClient;
 
-  constructor() {
-    const {
-      AWS_ACCESS_KEY_ID,
-      AWS_SECRET_ACCESS_KEY,
-      DYNAMODB_ENDPOINT,
-      DYNAMO_REGION,
-    } = process.env;
+  constructor(private configService: ConfigService) {
     this.client = new DynamoDBClient({
-      region: DYNAMO_REGION as string,
-      endpoint: DYNAMODB_ENDPOINT,
+      region: this.configService.get<string>('DYNAMO_REGION'),
+      endpoint: this.configService.get<string>('DYNAMODB_ENDPOINT'),
       credentials: {
-        accessKeyId: AWS_ACCESS_KEY_ID || '',
-        secretAccessKey: AWS_SECRET_ACCESS_KEY || '',
+        accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY_ID') || '',
+        secretAccessKey:
+          this.configService.get<string>('AWS_SECRET_ACCESS_KEY') || '',
       },
     });
 
