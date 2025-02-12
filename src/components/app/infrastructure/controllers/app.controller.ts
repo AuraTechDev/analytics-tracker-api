e2a87a7175cea675ce246fbe6fdbdcf14c5206ed
Erick Pac <erick.pac@helloiconic.com>
2025-02-11 23:14:20 -0600

    fix: correct typo in register method name in AppController

--- src/components/app/infrastructure/controllers/app.controller.ts
+++ src/components/app/infrastructure/controllers/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('register')
-  async regisrter(
+  async register(
     @Body()
     body: {
       name: string;
